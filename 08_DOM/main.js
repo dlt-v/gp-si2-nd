2b8c3d98230075b3deca046947dc94a32a773830
Tomek Michalski <tmm.michalski@gmail.com>
2022-04-03 10:35:56 +0200

    push more code

--- 08_DOM/main.js
+++ 08_DOM/main.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+//zapisywanie/cache'owanie elementów
+const przycisk = document.getElementById('przycisk');
+
+// dodawanie nasłuchiwacza na zdarzenia
+przycisk.addEventListener('click', przycikKlikniety);
+
+function przycikKlikniety() {
+    console.log('Zostałem kliknięty.');
+}
+
+/*
+ZADANIE
+Dodaj event listener dla podwójnego kliknięcia (dblclick) oraz wyloguj stosowny tekst.
+
+*/
