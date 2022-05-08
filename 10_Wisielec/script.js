edc3a2bbed33fdd1e7c0abe941151b60c490d7bf
Delta.v <tmm.michalski@gmail.com>
2022-05-08 10:25:10 +0200

    update

--- 10_Wisielec/script.js
+++ 10_Wisielec/script.js
@@ -20,12 +20,13 @@ const przyciskOk = document.querySelector('#ok');
 const przyciskReset = document.querySelector('#reset');
 const input = document.querySelector('#litera');
 const komunikat = document.querySelector('#komunikat');
-
+const obrazek = document.querySelector('img');
 przyciskOk.addEventListener('click', graj);
 
 let uzyteLitery = [];
 let zgadnieteLitery = [];
 let mozliweLitery = [];
+let zycia = listaObrazkow.length;
 const wyraz = listaWyrazow[Math.floor(Math.random() * listaWyrazow.length)];
 
 function main() {
@@ -34,7 +35,7 @@ function main() {
             mozliweLitery.push(wyraz[i]);
         }
     }
-    console.log(mozliweLitery);
+    //console.log(mozliweLitery);
     let wyrazDoDruku = [];
     let czyTrafiony = false;
     let liczbaSzans = listaObrazkow.length;
@@ -52,12 +53,17 @@ function graj() {
         return;
     } else {
         uzyteLitery.push(nowaLitera);
-        console.log(`Dodałeś ${nowaLitera} do użytych liter.`);
     }
 
     if (wyraz.includes(nowaLitera)) {
         zgadnieteLitery.push(nowaLitera);
+        komunikat.innerHTML = '';
+    } else {
+        // Gracz popełnij błąd.
+        komunikat.innerHTML = 'Ups! Tej litery nie ma w wyrazie.';
+        zycia -= 1; // zycia = zycia - 1;
     }
+    console.log(zycia);
 
     wyrazDoDruku = [];
     for (let i = 0; i < wyraz.length; i++) {
@@ -71,10 +77,25 @@ function graj() {
         komunikat.innerHTML = 'Wygrałeś!';
     }
     miejsceNaTekst.innerHTML = wyrazDoDruku.join(' ');
-    /*
-    Zadanie:
-    Dodaj warunek kiedy wszystkie litery zostały odkryte i poinformuj użytkownika.
-    */
 }
 
 main();
+
+/*
+Życia:
+- gracz ma ograniczoną ilość żyć. (11) DONE
+- za każdym błędem gracza musimy mu odjąć jedno życie.
+- musimy gracza poinformować kiedy skończyły mu się życia.
+- musimy zablokować możliwość zgadywania dalej, jeżeli graczowi skończył się życia.
+
+
+Przycisk Reset:
+- losować nowy wyraz
+- zresetować wyświetlany wyraz
+- zresetować życia, podanych i zgadniętych liter - reset zmiennych globalnych
+- (dodanie komunikatu o restarcie)
+- odblokowanie możliwości zgadywania
+
+Wynik Gracz vs komputer...
+
+*/
