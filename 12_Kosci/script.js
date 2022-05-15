da6950a0635dd0793a7b270b97ac9a0bfe9fd55c
Delta.v <tmm.michalski@gmail.com>
2022-05-15 10:46:54 +0200

    update

--- 12_Kosci/script.js
+++ 12_Kosci/script.js
@@ -1,4 +1,3 @@
-console.log('Hello');
 const obrazki = [
     'https://cdn.glitch.com/8fbc579f-3346-47a0-abbc-945a83abb962%2Fkosc-1.png?v=1610038358032',
 
@@ -12,3 +11,17 @@ const obrazki = [
 
     'https://cdn.glitch.com/8fbc579f-3346-47a0-abbc-945a83abb962%2Fkosc-6.png?v=1610038358193',
 ];
+
+// Zmienne globalne
+let punkty = [0, 0];
+let punktyRundy = 0;
+let aktualnyGracz = 0;
+let czyMoznaGrac = true;
+
+// Funkcja startowa
+function nowaGra() {
+    // Reset danych
+    // Elementy HTML
+    document.querySelector('#wynik-0').textContent = '0';
+}
+nowaGra();
